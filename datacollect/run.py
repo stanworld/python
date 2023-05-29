661de0421e0534853b0033adb88ee817fb01ad72
stanworld <songtancs@gmail.com>
2023-05-29 15:32:28 -0400

    read/write to excel file

--- datacollect/run.py
+++ datacollect/run.py
@@ -1,6 +1,18 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
+ 
+# read by default 1st sheet of an excel file
+inputfile = input("Enter the full name of input file: ")
+outputfile = input("Enter the full name of output file: ")
+inputsheet = input("Enter the name of input sheet: ")
+outputsheet = input("Enter the name of output sheet: ")
+df1 = pd.read_excel(inputfile, sheet_name=inputsheet)
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+print("Before processing: number of rows:" + str(len(df1)))
+df2 = df1[df1["Sample Status"] != "Abandoned" ]
+df2 = df2[df2["PF Bases (BC)"]/pow(10,9) > 9.5 ]
+df2 = df2[df2["Target Bases @ 10X % (HS)"] > 85 ]
+print("After processing: number of rows:"+ str(len(df2)))
+print("Selecting columns to generate new sheet")
+selected_columns = df2[['Project', 'Collaborator Sample ID']]
+selected_columns.to_excel(outputfile, sheet_name=outputsheet, index=False)
+print("Job Done!")
