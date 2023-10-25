838ea04a7f916cf9be85a7fdd77f0d7b6144e90c
Song Tan <songtancs@gmail.com>
2023-10-24 21:27:26 -0400

    create instance with ssh access and docker installed

--- project1/tf/main.tf
+++ project1/tf/main.tf
@@ -13,11 +13,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh"
+  description = "Allow SSH traffic"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
+  key_name      = "stanworldkey"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Project1AppServerInstance"
   }
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io
+              usermod -aG docker ubuntu
+              systemctl enable docker
+              systemctl start docker
+              EOF
 }
